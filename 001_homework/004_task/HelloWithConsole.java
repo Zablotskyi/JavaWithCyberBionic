f2b116d5c5572f57d1560809b85c23edd75c6ee2
Zablotskyi <zablotskyivv@gmail.com>
2025-11-14 21:16:38 +0200

    push lesson 1 and lesson 2

---
+++ 001_homework/004_task/HelloWithConsole.java
@@ -0,0 +1,9 @@
+public class HelloWithConsole {
+    public static void main(String[] args) {
+        String name = "Vasyl";
+        System.out.println("Hello " + name);
+    }
+    // in console write:
+    // javac HelloWithConsole.java - компіляція файлу
+    // java HelloWithConsole - запускаємо файл
+}
---
+++ 002_practice/src/Solution_04.java
@@ -0,0 +1,8 @@
+public class Solution_04 {
+    public static void main(String[] args) {
+        double diameter = 3;
+        double l = 3.14 * diameter;
+
+        System.out.println(diameter);
+    }
+}
