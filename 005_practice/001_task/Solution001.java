3a86f71525c82fa3bd04e170a4d8de34b95cf9c2
Zablotskyi <zablotskyivv@gmail.com>
2025-11-24 21:10:40 +0200

    upload lecture 005_Logic

---
+++ 005_practice/001_task/Solution001.java
@@ -0,0 +1,10 @@
+//Boolean1◦ . Дано ціле число A. Перевірити істинність висловлювання:
+//
+//«Число A є додатним».
+public class Solution001 {
+    public static void main(String[] args) {
+        int A = 3;
+        boolean result = A > 0;
+        System.out.println(result);
+    }
+}
